8ee62a4e4829cc4213417212910a0d48f38da12d
Esa Prakasa <esa.prakasa@gmail.com>
2018-12-20 11:02:58 +0700

    Menampilkan gambar dari webcam

---
+++ dispCam.cpp
@@ -0,0 +1,23 @@
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace cv;
+
+int main(int, char**)
+{
+    VideoCapture cap0(0);
+    if(!cap0.isOpened())
+        return -1;
+
+    Mat frame0;
+
+    for(;;)
+    {
+      system ("clear");
+      cap0 >> frame0;
+      imshow("Webcam view", frame0);
+      if(waitKey(30) >= 0) break;
+    }
+    return 0;
+}
+
+
